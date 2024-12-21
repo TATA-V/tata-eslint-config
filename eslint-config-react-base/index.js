362b92a393b890ff98e81b021b1a609bc167b36b
tatahyeonv <xhrl523@naver.com>
2024-12-21 17:47:51 +0900

    feat: eslint-config-react-base

---
+++ eslint-config-react-base/index.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  rules: {
+    'no-var': 'warn',
+    eqeqeq: 'warn',
+    'react/prop-types': 0,
+    'no-extra-semi': 'error',
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'import/prefer-default-export': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'react/jsx-pascal-case': 'warn',
+    'react/jsx-key': 'warn',
+    'no-debugger': 'off',
+    'react/function-component-definition': [
+      2,
+      { namedComponents: ['arrow-function', 'function-declaration'] },
+    ],
+    'react/react-in-jsx-scope': 0,
+    'react/prefer-stateless-function': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'no-nested-ternary': 0,
+    quotes: ['error', 'single'],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'arrow-parens': ['error', 'always'],
+    'no-console': 'warn',
+    'react/button-has-type': 'off',
+    'react/no-array-index-key': 'off',
+    'no-param-reassign': 'off',
+    'no-undef': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    semi: ['error', 'always'],
+    'react/jsx-no-useless-fragment': 'off',
+    'react/no-unused-prop-types': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
